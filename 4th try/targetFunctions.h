31c8f3955242c6b21f0cf1692d7fead81f190f11
Thomas <tschoenstein98@gmail.com>
2019-11-30 17:14:35 -0600

    Initial add
    Pickes up off shooting target 2

---
+++ 4th try/targetFunctions.h
@@ -0,0 +1,17 @@
+/*
+ * targetFunctions.h
+ *
+ *  Created on: Oct 10, 2019
+ *      Author: jdoe8
+ */
+
+#ifndef TARGETFUNCTIONS_H_
+#define TARGETFUNCTIONS_H_
+
+void sensorRead(void);
+void initADC(void);
+void initGPIO(void);
+void ADCoutputLED(uint32_t dataIn);
+
+
+#endif /* TARGETFUNCTIONS_H_ */
